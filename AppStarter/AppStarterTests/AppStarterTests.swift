17a37650edb60a2047eb4ca560c017645001e43c
Fred Strout <37194417+FStrout@users.noreply.github.com>
2026-03-24 11:48:57 -0500

    Initial commit
    This marks the start of this project

---
+++ AppStarter/AppStarterTests/AppStarterTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppStarterTests.swift
+//  AppStarterTests
+//
+//  Created by Fred Strout on 3/24/26.
+//
+
+import Testing
+@testable import AppStarter
+
+struct AppStarterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
